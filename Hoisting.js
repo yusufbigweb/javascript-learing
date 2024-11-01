82609f9c320683091269b351cbb46c22b82283d1
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-01 22:02:03 +0000

    Taddy Smith - by Hoisting

--- Hoisting.js
+++ Hoisting.js
@@ -1,8 +1,24 @@
-console.log(name) // one error here: Uncaught ReferenceError: Cannot access 'name' before initialization
-const name = "Yusuf";
+// fun()
+// console.log(name) // one error here: Uncaught ReferenceError: Cannot access 'name' before initialization
 
 function fun(){
     console.log("Yusuf'S Functions")
 }
-fun()
+const name = "Yusuf";
 // The Hoisting is the simple word memory and primitive data and non-primitive data
+
+
+
+foo();
+bar();
+
+
+//function declaration work! 
+function foo(){
+    console.log(10);
+}
+
+// function expresstion stop hoisting!
+let bar = function(){
+    console.log(5)
+}
