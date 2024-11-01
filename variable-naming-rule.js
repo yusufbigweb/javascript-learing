52b9f13bca96ff824786941ec1082538d49d7fe4
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-01 23:19:15 +0000

    Learn with Shajeel by Variable Naming Rule

---
+++ variable-naming-rule.js
@@ -0,0 +1,10 @@
+// Valid Variable in JavaScript 
+
+let firstName;
+let _lastName;
+let $fullName;
+// let 12Name; this is not valid variable. 
+let name12; //this is vaild varible.
+
+// let name yusuf = "yusuf" // space not vaild variable.
+// let var; // not used Reserd keywords like function, if, catch, try and more keyword related.
