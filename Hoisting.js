0f4df6f4c48a9a44fb0ab059ea955ce1281f6657
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-01 21:41:47 +0000

    Update

--- Hoisting.js
+++ Hoisting.js
@@ -1,8 +1,8 @@
+console.log(name) // one error here: Uncaught ReferenceError: Cannot access 'name' before initialization
 const name = "Yusuf";
 
 function fun(){
     console.log("Yusuf'S Functions")
 }
-
 fun()
-console.log(name)
+// The Hoisting is the simple word memory and primitive data and non-primitive data
