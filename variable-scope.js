8ad72d7254ba8236dcc54361a43baef059e6ac6c
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-02 14:12:58 +0000

    Bro Code by variable scope

---
+++ variable-scope.js
@@ -0,0 +1,32 @@
+// Variable Scope in JavaScript
+
+
+// variable scope => where a variable is recognized and accessible (local & global)
+
+// local scope
+function function1(){
+    let x = 1; // local used variable
+    console.log(x)
+}
+
+function function2(){
+    let x = 2;
+    console.log(x)
+}
+
+
+
+
+// global scope
+
+const y = "ysuf";// global used variable
+
+function4()
+
+function function3(){
+    console.log(y)
+}
+
+function function4(){
+    console.log(y)
+}
